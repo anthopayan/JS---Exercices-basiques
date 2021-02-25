f38af0a96a217f876fd6c73c152b18de181df74d
anthopayan <anthony.payan08@outlook.fr>
2021-02-26 00:39:11 +0100

    work5

--- script_7.js
+++ script_7.js
@@ -1,11 +1,10 @@
 function Heybot() {
 var phr;
-
 while (true) {
     phr = prompt('Ecris une phrase :');
-    //a = phr.length + 1;
     letter = phr.substr(phr.length-1,1);
     mphr = phr.toUpperCase();
+    
 
     if (letter === "?") {
         console.log('Ouais Ouais...');
@@ -15,11 +14,15 @@ while (true) {
         console.log('Pwa, calme-toi...');
     } 
 
+    if (phr.includes("Fortnite") || phr.includes("fortnite")) {
+        console.log("on s' fait une partie soum-soum ?");
+    } 
+
     if (phr == "") {
         console.log("t'es en PLS ?");
     } 
 
-    if (phr != "" && phr != mphr && letter != "?"){
+    if (phr != "" && phr != mphr && letter != "?" && (phr.includes("Fortnite") || phr.includes("fortnite") == false)){
         console.log("balek");
     }
 
