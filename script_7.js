9be0381f20d81475695b03e84de71e9da2a048c5
anthopayan <anthony.payan08@outlook.fr>
2021-02-26 00:20:29 +0100

    work4

--- script_7.js
+++ script_7.js
@@ -5,12 +5,29 @@ while (true) {
     phr = prompt('Ecris une phrase :');
     //a = phr.length + 1;
     letter = phr.substr(phr.length-1,1);
-    
+    mphr = phr.toUpperCase();
+
     if (letter === "?") {
         console.log('Ouais Ouais...');
     } 
-    else { // on rentre dans ce else si letter est vide (l'utilisateur ne saisit rien)
-        break; // On quitte la boucle
+
+    if (phr == mphr && phr != "") {
+        console.log('Pwa, calme-toi...');
+    } 
+
+    if (phr == "") {
+        console.log("t'es en PLS ?");
+    } 
+
+    if (phr != "" && phr != mphr && letter != "?"){
+        console.log("balek");
     }
+
+    else {
+        break; // On quitte la boucle
+        
+        
+    }
+    
 }
 }
